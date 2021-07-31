95598660074ba73b6b1c35d3d35dddcff6674987
x0candy <2697611383@qq.com>
2021-07-31 19:33:07 +0800

    update

---
+++ vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login {
+  background-color: rgb(31, 236, 185);
+  height: 100%;
+}
+</style>

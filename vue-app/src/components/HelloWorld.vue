d86aa93824911b398870045c22f8f56faff385bc
x0candy <2697611383@qq.com>
2021-08-01 19:19:16 +0800

    update

--- vue-app/src/components/HelloWorld.vue
+++ vue-app/src/components/HelloWorld.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="login">
-    {{msg}}
+    <div class="login-form">
+      <el-form ref="formData"
+               :model="formData"
+               label-width="80px">
+        <el-form-item label="用户名">
+          <el-input v-model="formData.userName"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="formData.passWorld"></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -9,7 +20,10 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      formData: {
+        userName: '',
+        passWorld: ''
+      }
     }
   }
 }
@@ -20,5 +34,12 @@ export default {
 .login {
   background-color: rgb(31, 236, 185);
   height: 100%;
+}
+.login-form {
+  width: 20%;
+  position: relative;
+  left: 50%;
+  top: 50%;
+  transform: translate();
 }
 </style>
--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+axios.defaults.url = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 /* eslint-disable no-new */
